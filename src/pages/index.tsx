bbdb1ef4ed6af9b0db1efce23b32a8feacd1a06d
biagavirete <biagavirete@gmail.com>
2021-02-23 10:22:52 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+import { ExperienceBar } from '../components/ExperienceBar';
+
+function App() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default App;
